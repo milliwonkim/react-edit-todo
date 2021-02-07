ec7f5d83aee761354798237c45c4508df6e6eb46
milliwonkim <rldnjs9347@gmail.com>
2021-02-07 19:58:10 +0900

    [fix] fix icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-// import _ from 'lodash';
+import Delete from './delete.svg';
+import Check from './check.svg';
+import Checklist from './checklist.svg';
+import Close from './close.svg';
+import Plus from './plus.svg';
 import './App.css';
 
 function App() {
@@ -57,22 +61,15 @@ function App() {
   }
 
   const isCompleted = (key) => {
-    console.log('key of isCompleted', key)
     let selectedItems = items.map(item => {
       if(item.key === key) {
         item.isCompleted = !item.isCompleted;
       }
-      console.log(item, 'item of selectItems')
       return item;
     })
 
     setItems(selectedItems);
   };
-
-  // const filterCompleted = (e) => {
-  //   console.log(e.target.value)
-  //   setCompleted(e.target.value)
-  // }
 
   const listItems = items.filter(item => {
     if(completed === 'true') {
@@ -89,12 +86,8 @@ function App() {
               value={item.text}
               onChange={(e) => setUpdate(e.target.value, item.key)}
             />
-            <span>
-              <button onClick={() => deleteItem(item.key)}>Delete</button>
-            </span>
-            <span>
-              <button onClick={() => isCompleted(item.key)}>Completed</button>
-            </span>
+            <StyledImg src={Delete} alt="delete" onClick={() => deleteItem(item.key)} />
+            <StyledImg src={Check} alt='check' onClick={() => isCompleted(item.key)} />
           </StyledP1>
         </StyledDiv2>
     )
@@ -102,22 +95,21 @@ function App() {
 
   return (
     <StyledDiv1 className="app">
-      <select onChange={(e) => setCompleted(e.target.value)}>
-        <option value='false'>Not Completed</option>
-        <option value='true'>Completed</option>
-      </select>
+      <StyledDiv5>
+        <StyledButton2><StyledImg src={Checklist} onClick={() => setCompleted('true')} /></StyledButton2>
+        <StyledButton2><StyledImg src={Close} onClick={() => setCompleted('false')} /></StyledButton2>
+      </StyledDiv5>
       <form onSubmit={addItem}>
         <StyledDiv4>
           <StyledP1>
-          <input
+          <StyledInput2
             type='text'
             placeholder='Enter Text'
             value={currentItem.text}
             onChange={handleInput}
           />
-          <span>
-            <button>Add</button>
-          </span>
+          <StyledButton1><StyledImg src={Plus} /></StyledButton1>
+          {/* <button>Add</button> */}
           </StyledP1>
         </StyledDiv4>
       </form>
@@ -132,44 +124,84 @@ export default App;
 
 const StyledDiv1 = styled.div`
   border-radius: 8px;
-  box-shadow: 10px 10px 50px 1px #0984e3;
+  box-shadow: 10px 10px 50px 1px #636e72;
   margin: 34px auto;
   max-width: 900px;
   height: 800px;
   padding: 20px;
   border: transparent;
-  `;
-  
+`;
+
 const StyledDiv2 = styled.div`
   max-width: 572px;
   border-radius: 8px;
-  border: 1px solid black;
+  border: transparent;
   margin: 34px 30px;
   padding: 3px 3px;
-  `;
-  
-  const StyledDiv3 = styled.div`
+  box-shadow: 10px 10px 50px 1px #636e72;
+`;
+
+const StyledDiv3 = styled.div`
   border-radius: 8px;
-  border: 1px solid black;
+  border: transparent;
   margin: 34px auto;
   max-width: 635px;
-  max-height: 596px;
+  height: 596px;
   overflow: scroll;
-  `;
+  box-shadow: 10px 10px 50px 1px #636e72
+`;
 
-  const StyledDiv4 = styled.div`
-    max-width: 633px;
-    border-radius: 8px;
-    border: 1px solid black;
-    margin: 34px auto;
-    padding: 3px 3px;
-    `;
-  
-  const StyledP1 = styled.p`
+const StyledDiv4 = styled.div`
+  max-width: 633px;
+  border-radius: 8px;
+  border: transparent;
+  margin: 34px auto;
+  padding: 3px 3px;
+  box-shadow: 10px 10px 50px 1px #636e72
+`;
+
+const StyledDiv5 = styled.div`
+  max-width: 633px;
+  margin: 10px auto;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledP1 = styled.p`
   text-align: center;
   min-width: 300px;
 `;
 
 const StyledInput1 = styled.input`
   min-width: 200px;
+  height: 30px;
+  margin: 0 10px;
+`;
+
+const StyledInput2 = styled.input`
+  min-width: 240px;
+  height: 30px;
+  margin: 0 10px;
+`;
+
+const StyledImg = styled.img`
+  display: inline-block;
+  vertical-align: middle;
+  margin: 0px 10px;
+  width: 30px;
+`;
+
+const StyledButton1 = styled.button`
+  display: inline-block;
+  background-color: transparent;
+  border: transparent;
+  outline: 0;
+`;
+
+const StyledButton2 = styled.button`
+  display: block;
+  text-align: center;
+  background-color: transparent;
+  border: transparent;
+  outline: 0;
 `;
