58d3af1c83ddda1284f74b61630e123b96174fba
milliwonkim <rldnjs9347@gmail.com>
2021-02-07 15:34:26 +0900

    [add] adding functionalities: filtering todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+// import _ from 'lodash';
 import './App.css';
 
 function App() {
 
+  const [completed, setCompleted] = useState('');
   const [items, setItems] = useState([]);
   const [currentItem, setCurrentItem] = useState({
     text: '',
-    key: ''
+    key: '',
+    isCompleted: false
   })
+
 
   const addItem = (e) => {
     e.preventDefault();
     if(currentItem.text !== '') {
       const newItems = [...items, currentItem];
       setItems(newItems);
-      console.log('items', items)
       setCurrentItem({
         text: '',
-        key: ''
+        key: '',
+        isCompleted: false
       })
     }
   };
@@ -26,6 +31,7 @@ function App() {
     setCurrentItem({
       text: e.target.value,
       key: Date.now(),
+      isCompleted: false
     })
   }
 
@@ -50,33 +56,120 @@ function App() {
     setItems(filteredItems);
   }
 
-  const listItems = items.map((item) => {
-    return (
-      <div key={item.key}>
-          <input
-            type='text'
-            value={item.text}
-            onChange={(e) => setUpdate(e.target.value, item.key)}
-          />
-          <button onClick={() => deleteItem(item.key)}>Delete</button>
-      </div>
+  const isCompleted = (key) => {
+    console.log('key of isCompleted', key)
+    let selectedItems = items.map(item => {
+      if(item.key === key) {
+        item.isCompleted = !item.isCompleted;
+      }
+      console.log(item, 'item of selectItems')
+      return item;
+    })
+
+    setItems(selectedItems);
+  };
+
+  // const filterCompleted = (e) => {
+  //   console.log(e.target.value)
+  //   setCompleted(e.target.value)
+  // }
+
+  const listItems = items.filter(item => {
+    if(completed === 'true') {
+      return item.isCompleted === true;
+    } else {
+      return item.isCompleted === false;
+    }
+  }).map((item) => {
+      return (
+        <StyledDiv2 key={item.key}>
+          <StyledP1>
+            <StyledInput1
+              type='text'
+              value={item.text}
+              onChange={(e) => setUpdate(e.target.value, item.key)}
+            />
+            <span>
+              <button onClick={() => deleteItem(item.key)}>Delete</button>
+            </span>
+            <span>
+              <button onClick={() => isCompleted(item.key)}>Completed</button>
+            </span>
+          </StyledP1>
+        </StyledDiv2>
     )
-  })
+  });
 
   return (
-    <div className="app">
+    <StyledDiv1 className="app">
+      <select onChange={(e) => setCompleted(e.target.value)}>
+        <option value='false'>Not Completed</option>
+        <option value='true'>Completed</option>
+      </select>
       <form onSubmit={addItem}>
-        <input
-          type='text'
-          placeholder='Enter Text'
-          value={currentItem.text}
-          onChange={handleInput}
-        />
-        <button>Add</button>
+        <StyledDiv4>
+          <StyledP1>
+          <input
+            type='text'
+            placeholder='Enter Text'
+            value={currentItem.text}
+            onChange={handleInput}
+          />
+          <span>
+            <button>Add</button>
+          </span>
+          </StyledP1>
+        </StyledDiv4>
       </form>
-      {listItems}
-    </div>
+      <StyledDiv3>
+        {listItems}
+      </StyledDiv3>
+    </StyledDiv1>
   );
 }
 
 export default App;
+
+const StyledDiv1 = styled.div`
+  border-radius: 8px;
+  box-shadow: 10px 10px 50px 1px #0984e3;
+  margin: 34px auto;
+  max-width: 900px;
+  height: 800px;
+  padding: 20px;
+  border: transparent;
+  `;
+  
+const StyledDiv2 = styled.div`
+  max-width: 572px;
+  border-radius: 8px;
+  border: 1px solid black;
+  margin: 34px 30px;
+  padding: 3px 3px;
+  `;
+  
+  const StyledDiv3 = styled.div`
+  border-radius: 8px;
+  border: 1px solid black;
+  margin: 34px auto;
+  max-width: 635px;
+  max-height: 596px;
+  overflow: scroll;
+  `;
+
+  const StyledDiv4 = styled.div`
+    max-width: 633px;
+    border-radius: 8px;
+    border: 1px solid black;
+    margin: 34px auto;
+    padding: 3px 3px;
+    `;
+  
+  const StyledP1 = styled.p`
+  text-align: center;
+  min-width: 300px;
+`;
+
+const StyledInput1 = styled.input`
+  min-width: 200px;
+`;
